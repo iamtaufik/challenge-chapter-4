cb23cbbd1ff54c7785fa11cbcf60da391c48f103
iamtaufik <muhtopik07@gmail.com>
2023-10-07 15:46:29 +0700

    feat: account controllers

--- validations/accounts.validation.js
+++ validations/accounts.validation.js
@@ -3,7 +3,7 @@ const joi = require('joi');
 const createAccountSchema = joi.object({
   user_id: joi.number().required(),
   bank_name: joi.string().required(),
-  bank_account_number: joi.number().required(),
+  bank_account_number: joi.string().required(),
   balance: joi.number().required(),
 });
 
